180da302695a79a4b2b6e8abd520f56ea761e1b2
vadik likholetov <vadikas@gmail.com>
2024-07-01 21:02:27 +0300

    ifix

--- pkgs/l4t/default.nix
+++ pkgs/l4t/default.nix
@@ -140,6 +140,7 @@ let
       echo NvOsLibraryLoad NvOsLibraryLoad_3d > $remapFile
 
       #remove broken libcuda links
+      echo remove broken libcuda links
       rm ./lib/libcuda.so
       
       for lib in $(find ./lib -name "*.so*"); do
