1891b1b3cf3b6fd6334c472f21108f3e68e86a2e
Tanel Dettenborn <tanel@ssrc.tii.ae>
2024-12-09 12:18:03 +0200

    Kernel USB changes
    * Built-in USB_UAS driver
    * Include USB firmware into kernel
    
    Signed-off-by: Tanel Dettenborn <tanel@ssrc.tii.ae>

--- kernel/default.nix
+++ kernel/default.nix
@@ -86,11 +86,13 @@ pkgsAarch64.buildLinux (args // {
     # TODO Still needed?
     # if USB_XHCI_TEGRA is built as module, the kernel won't build
     {
-      name = "make-USB_XHCI_TEGRA-builtins";
+      name = "make-USB_XHCI_TEGRA-builtins with firmware";
       patch = null;
       extraConfig = ''
         USB_XHCI_TEGRA y
-      '';
+        EXTRA_FIRMWARE_DIR ${l4t-xusb-firmware}/lib/firmware
+        EXTRA_FIRMWARE nvidia/tegra194/xusb.bin
+  '';
     }
 ] ++ kernelPatches;
 
@@ -105,28 +107,12 @@ pkgsAarch64.buildLinux (args // {
 
     # Following modules need for iso_minimal
     ISO9660 = module;
-    USB_UAS = module;
+    USB_UAS = yes;
 
     # Required by IO base b
     USB_ONBOARD_HUB = no;
 
     # TODO: Are below options still needed??
-
-    #  MODPOST modules-only.symvers
-    #ERROR: modpost: "xhci_hc_died" [drivers/usb/host/xhci-tegra.ko] undefined!
-    #ERROR: modpost: "xhci_hub_status_data" [drivers/usb/host/xhci-tegra.ko] undefined!
-    #ERROR: modpost: "xhci_enable_usb3_lpm_timeout" [drivers/usb/host/xhci-tegra.ko] undefined!
-    #ERROR: modpost: "xhci_hub_control" [drivers/usb/host/xhci-tegra.ko] undefined!
-    #ERROR: modpost: "xhci_get_rhub" [drivers/usb/host/xhci-tegra.ko] undefined!
-    #ERROR: modpost: "xhci_urb_enqueue" [drivers/usb/host/xhci-tegra.ko] undefined!
-    #ERROR: modpost: "xhci_irq" [drivers/usb/host/xhci-tegra.ko] undefined!
-    #USB_XHCI_TEGRA = module;
-    USB_XHCI_TEGRA = yes;
-
-    # stage-1 links /lib/firmware to the /nix/store path in the initramfs.
-    # However, since it's builtin and not a module, that's too late, since
-    # the kernel will have already tried loading!
-    EXTRA_FIRMWARE_DIR = freeform "${l4t-xusb-firmware}/lib/firmware";
 
     # Override the default CMA_SIZE_MBYTES=32M setting in common-config.nix with the default from tegra_defconfig
     # Otherwise, nvidia's driver craps out
