52f24af38b13c73606bd660015b048b03e32cb44
Brian McGillion <bmg.avoin@gmail.com>
2025-01-26 10:57:07 +0400

    TMP add the new fw
    to be fixed
    
    Signed-off-by: Brian McGillion <bmg.avoin@gmail.com>
    Co-authored-by: Tanel Dettenborn <tanel.dettenborn@unikie.com>

---
+++ pkgs/uefi-firmware/uefi-firmware-36.4/edk2-pytool-extensions.nix
@@ -0,0 +1,52 @@
+{
+  buildPythonPackage,
+  fetchFromGitHub,
+  setuptools,
+  setuptools-scm,
+  python3Packages,
+  pyyaml,
+  edk2-pytool-library,
+  pefile,
+  gitpython,
+  openpyxl,
+  xlsxwriter,
+}:
+
+buildPythonPackage rec {
+  pname = "edk2-pytool-extensions";
+  version = "0.28.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "tianocore";
+    repo = "edk2-pytool-extensions";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-VmYCiqQrznjg1uP31MWO70cnh4EiNdxLENBpA0dUGu8=";
+  };
+
+  patches = [
+    ./0001-Remove-nuget-download-and-execute-it-without-Mono.patch
+  ];
+
+  SETUPTOOLS_SCM_PRETEND_VERSION = version;
+  doCheck = false;
+
+  buildInputs = [
+    setuptools
+    setuptools-scm
+  ];
+
+  dependencies = [
+    edk2-pytool-library
+    pefile
+    gitpython
+    openpyxl
+    xlsxwriter
+  ];
+
+  propagatedBuildInputs = [
+    pyyaml
+    setuptools
+    python3Packages.semantic-version
+  ];
+}
