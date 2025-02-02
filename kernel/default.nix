33dd57c3d3162f8bfd77f42426dc51aacafd7f41
Tanel Dettenborn <tanel@ssrc.tii.ae>
2025-02-02 22:31:24 +0200

    Select kernel version based on Jetson BSP version
    Signed-off-by: Tanel Dettenborn <tanel@ssrc.tii.ae>

--- kernel/default.nix
+++ kernel/default.nix
@@ -1,5 +1,10 @@
-#
-##TODO
-#
-# Placeholder
-#
+{ l4tVersion, l4t-xusb-firmware, realtime ? false, config, lib, pkgs, ... }:
+let
+  kernel-file = if l4tVersion == "36.4.3" then
+      ./5.15-r36-4-3
+  else if l4tVersion == "35.6.0" then
+      ./5.10-r35.6
+  else
+      throw "Not supported l4tVersion version";
+in
+pkgs.callPackage "${kernel-file}" { inherit l4t-xusb-firmware realtime; kernelPatches = []; }
