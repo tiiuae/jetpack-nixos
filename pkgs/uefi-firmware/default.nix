51560bba70cc6f255c8a3a52ca5d2b058b3923ca
Daniel Fullmer <dfullmer@anduril.com>
2025-03-28 17:54:08 -0700

    uefi-firmware: update for 35.6.1

--- pkgs/uefi-firmware/default.nix
+++ pkgs/uefi-firmware/default.nix
@@ -2,7 +2,7 @@
 let
   uefi-firmware-file = if l4tVersion == "36.4.3" then
       ./r36.4.3
-  else if l4tVersion == "35.6.0" then
+  else if l4tVersion == "35.6.1" then
       ./r35.6
   else
       throw "Not supported l4tVersion version";
