6e543867b2d1f429de88275658d7638c67df4389
Tanel Dettenborn <tanel@ssrc.tii.ae>
2025-03-31 00:34:46 +0300

    Switch from nvidia BSP 5.15 kernel to upstream 6.6 kernel
    Signed-off-by: Tanel Dettenborn <tanel@ssrc.tii.ae>

--- kernel/default.nix
+++ kernel/default.nix
@@ -1,7 +1,8 @@
 { l4tVersion, l4t-xusb-firmware, realtime ? false, config, lib, pkgs, kernelPatches ? [], ... }:
 let
   kernel-file = if l4tVersion == "36.4.3" then
-      ./5.15-r36-4-3
+      #./5.15-r36-4-3
+      ./6.6-upstream
   else if l4tVersion == "35.6.0" then
       ./5.10-r35.6
   else
