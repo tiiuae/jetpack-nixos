619be23e57fec423762a94ae0c20bea87a7c5891
Daniel Fullmer <dfullmer@anduril.com>
2025-04-11 14:27:43 -0700

    kernel: update for 35.6.1

--- kernel/default.nix
+++ kernel/default.nix
@@ -3,7 +3,7 @@ let
   kernel-file = if l4tVersion == "36.4.3" then
       #./5.15-r36-4-3
       ./6.6-upstream
-  else if l4tVersion == "35.6.0" then
+  else if l4tVersion == "35.6.1" then
       ./5.10-r35.6
   else
       throw "Not supported l4tVersion version";
