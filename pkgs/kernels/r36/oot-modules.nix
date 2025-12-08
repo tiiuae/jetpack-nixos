41af2e2786dadcb327afb36168b80a45d22645e8
Tanel Dettenborn <tanel.dettenborn@tii.ae>
2025-12-08 14:41:52 +0200

    Upstream kernel 6.6 and kernel selection option
    Signed-off-by: Tanel Dettenborn <tanel.dettenborn@tii.ae>

--- pkgs/kernels/r36/oot-modules.nix
+++ pkgs/kernels/r36/oot-modules.nix
@@ -31,6 +31,8 @@ let
         ./patches/nvidia-oot/0005-Fix-header-guard-in-halfrf_ops_rtl8852c.h.patch
         ./patches/nvidia-oot/0001-crypto-tegra-Disable-softirqs-before-finalizing-requ.patch
         ./patches/nvidia-oot/0001-Lower-priority-of-tegra-se-crypto.patch
+        ./patches/nvidia-oot/0001-Fix-register_shrinker_has_fmt_arg-conftest.patch
+        ./patches/nvidia-oot/0001-tegra-fbdev-take-the-core-allocated-fb_info-on-Linux.patch
       ];
     };
     nvgpu = gitRepos.nvgpu;
